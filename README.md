1dd74453b2453deb2a9932b321e37b1e71a789e0
x-Sziklai <x-sziklaib@wizzair.com>
2017-12-12 17:26:46 +0100

    initial setup

---
+++ README.md
@@ -0,0 +1,4 @@
+Webpack 3 & Foundation 6 
+===========
+
+Playground for module based foundation w. webpack and babel 
---
+++ app/index.js
@@ -0,0 +1,8 @@
+import Foundation from './foundation';
+import 'styles/index.scss';
+
+jQuery(document).ready(() => {
+
+  jQuery(document).foundation();
+});
+
